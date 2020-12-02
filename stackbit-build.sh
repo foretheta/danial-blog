1c7d76aaf8ec3277f33a241e4a0c4bbf51be8f2a
Danial Ranjha <danial@foretheta.com>
2020-12-01 23:47:04 -0800

    Update stackbit-build.sh

--- stackbit-build.sh
+++ stackbit-build.sh
@@ -10,7 +10,8 @@ if [[ -z "${STACKBIT_API_KEY}" ]]; then
 else
     npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/5e63439f14b69200127309bf 
 fi
-cat /opt/build/repo/src/data/data.json
+ls -alh /opt/build/repo/src/pages/posts/
+
 curl -s -X POST https://api.stackbit.com/project/5e63439f14b69200127309bf/webhook/build/ssgbuild > /dev/null
 gatsby build
 curl -s -X POST https://api.stackbit.com/project/5e63439f14b69200127309bf/webhook/build/publish > /dev/null
